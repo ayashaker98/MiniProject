ce85a6c2f003a709a5e892849f0a78ec2fbd36aa
Noha Mohamed <noha158@aucegypt.edu>
2019-11-08 21:49:01 +0200

    updated

--- MiniProjectParser.py
+++ MiniProjectParser.py
@@ -5,7 +5,8 @@ from collections import namedtuple
 import array
 import numpy
 from scipy.spatial.distance import euclidean
-from scipy.interpolate import interp1d, interp2d
+from scipy import arange, array, exp
+from scipy.interpolate import interp1d, interp2d, InterpolatedUnivariateSpline
 liberty_file = "/Users/noha/Desktop/osu035.lib"
 library = parse_liberty(open(liberty_file).read())
 fname = "/Users/noha/Desktop/rca4.rtlnopwr.v"
@@ -13,7 +14,7 @@ fname = "/Users/noha/Desktop/rca4.rtlnopwr.v"
 import re
 
 maxFanout = 3 #input("Please Enter the maximum Fanout for any cell : ") 
-maxDelay = 0.01 #input("Please Enter the maximum Delay for any cell : ") 
+maxDelay = 0.23 #input("Please Enter the maximum Delay for any cell : ") 
 
 def findMiddle(input_list):
     return(int((len(input_list) - 1)/2))
@@ -183,8 +184,12 @@ rise_delay = -1
 fall_delay = -1
 minIndex1 = 0
 minIndex2 = 0
+o = 0
 distance = []
-cell_delay_things = namedtuple('cell_delay_things', ['cell', 'delay'])
+distance2 = []
+cell_delay_things = namedtuple('cell_delay_things', ['cell', 'delay', 'status'])
+cell_delay_rise = []
+cell_delay_fall = []
 cell_delay = []
 
 for i in range(len(CellwithLoad)):
@@ -200,31 +205,47 @@ for i in range(len(CellwithLoad)):
     timing_table_transition_fall = timing_table_fall.get_array("index_1")
     timing_table_capacitance_fall = timing_table_fall.get_array("index_2")
     timing_table_values_fall = timing_table_fall.get_array("values")
+    #For Rise
     for j in range(len(timing_table_capacitance_rise[0])):
         if (CellwithLoad[i].load == timing_table_capacitance_rise[0][j]):
             rise_delay = timing_table_values_rise[findMiddle(timing_table_transition_rise[0])][j]
             print("Rise Delay was set successfuly")
     if(rise_delay == -1):
         # print("Rise Delay not set, must do interpolation or extrapolation!!!")
-        for k in range(len(timing_table_capacitance_rise[0])):
-            distance.append(euclidean(CellwithLoad[i].load,timing_table_capacitance_rise[0][k]))
-        minIndex1 = numpy.argmin(distance)
-        distance[minIndex1] = 999999
-        minIndex2 = numpy.argmin(distance)
-        # print(minIndex1)
-        distance.clear()
-        xvals = [timing_table_capacitance_rise[0][minIndex1], timing_table_capacitance_rise[0][minIndex2]]
-        yvals = [timing_table_transition_rise[0][findMiddle(timing_table_transition_rise[0])]]
-        # print(yvals)
-        fvals = [timing_table_values_rise[findMiddle(timing_table_transition_rise[0])][minIndex1], timing_table_values_rise[findMiddle(timing_table_transition_rise[0])][minIndex2]]
+        xvals = timing_table_capacitance_rise[0]
+        yvals = [timing_table_transition_rise[0][2]]
+        fvals = timing_table_values_rise[findMiddle(timing_table_transition_rise[0])]
+        interp_rise = InterpolatedUnivariateSpline(xvals, fvals)
+        # print("RISE")
+        # print(interp_rise(CellwithLoad[i].load))
+        cell_delay_rise.append(cell_delay_things(CellwithLoad[i].cell,interp_rise(CellwithLoad[i].load), 'none'))
 
-        # print(xvals)
-        # print(yvals)
-        # print(fvals)
-        # print(fvals)
-        print(interp2d(xvals, yvals, fvals))
+    #For Fall
+    for j in range(len(timing_table_capacitance_fall[0])):
+        if (CellwithLoad[i].load == timing_table_capacitance_fall[0][j]):
+            fall_delay = timing_table_values_fall[findMiddle(timing_table_transition_fall[0])][j]
+            print("Fall Delay was set successfuly")
 
-        # cell_delay.append(cell_delay_things(CellwithLoad[i].cell,interp2d(xvals,findMiddle(timing_table_transition_rise[0]), fvals)))
+    if(fall_delay == -1):
+        # print("Fall Delay not set, must do interpolation or extrapolation!!!")
+        xvals_fall = timing_table_capacitance_fall[0]
+        yvals_fall = [timing_table_transition_fall[0][2]]
+        fvals_fall = timing_table_values_fall[findMiddle(timing_table_transition_fall[0])]
+        interp_fall = InterpolatedUnivariateSpline(xvals_fall, fvals_fall)
+        # print("FALL")
+        # print(interp_fall(CellwithLoad[i].load))
+        cell_delay_fall.append(cell_delay_things(CellwithLoad[i].cell,interp_fall(CellwithLoad[i].load), 'none'))
 
 
+for q in range(len(cell_delay_fall)):
+    maxFallRise = max(cell_delay_fall[q].delay, cell_delay_rise[q].delay)
+    cell_delay.append(cell_delay_things(cell_delay_fall[q].cell, maxFallRise, 'none'))
 
+for g in range(len(cell_delay)):
+    if (cell_delay[g].delay > float(maxDelay)):
+        cell_delay[g] = cell_delay[g]._replace(status = "Violating")
+    else:
+        cell_delay[g] = cell_delay[g]._replace(status = "Not Violating")
+
+#cell_delay has the final delay of each cell with a space saying if its violated or not
+#Fanout has the final fanout of each cell with a space saying if its violated or not
